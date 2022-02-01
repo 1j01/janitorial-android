f9b3196bf9ce3c5892505435db9c83d907e9e45b
Isaiah Odhner <isaiahodhner@gmail.com>
2022-02-01 01:04:58 -0500

    Configure banner comment generators to match my template

--- src/game.js
+++ src/game.js
@@ -5917,9 +5917,9 @@ const gatherStatistics = async (originalOnly) => {
 	return { levelsPerEntityType, occurrencesPerEntityType };
 };
 // eslint-disable-next-line no-unused-vars
-const renderBannerComment = (sectionName, sectionDescription = "") => {
+const renderBannerComment = (sectionName) => {
 	// This generates a code heading comment, using the game's pixel font.
-	// You can use `console.log(renderBannerComment("META", "some description"))`
+	// You can use `console.log(renderBannerComment("Meta"))`
 	// to generate a comment, but I use a VS Code extension Banner Comment + to do it quicker.
 	// I made the font into a figlet font in order to be compatible; see below FIGlet font generation.
 	const canvas = document.createElement("canvas");
@@ -5936,11 +5936,22 @@ const renderBannerComment = (sectionName, sectionDescription = "") => {
 		}
 		textArt += "\n";
 	}
-	return `//\n${textArt.replace(/\s+$/gm, "")}\n//\n//SECTION: ${sectionName} - ${sectionDescription}\n//`;
+	return `
+// #${""}endregion
+//
+${textArt.replace(/\s+$/gm, "")}
+//
+// #${""}region ${sectionName}
+`;
 };
 // eslint-disable-next-line no-unused-vars
 const renderFIGletFont = () => {
 	// Generate a FIGlet font (.flf) from the Junkbot font.
+	// This can be used with the Banner Comment + VS Code extension,
+	// or other software.
+	// A configuration is provided in the workspace to generate banner comments in this project's style.
+	// It should work automatically if you install the extension:
+	// https://marketplace.visualstudio.com/items?itemName=lunarlimbo.banner-comments-plus
 	const canvas = document.createElement("canvas");
 	const ctx = canvas.getContext("2d");
 	const requiredCodes = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 196, 214, 220, 223, 228, 246, 252];
