d25797ba9be48d939f81dc8a982a8370eb0650ae
Isaiah Odhner <isaiahodhner@gmail.com>
2022-03-14 16:10:27 -0400

    Fix lint in CSS3D JS

--- css3d/src/css3d.js
+++ css3d/src/css3d.js
@@ -1,4 +1,6 @@
-// Purely for debugging purposes... and viewing the scene in a decent way
+// This script is purely for debugging purposes... and viewing the scene in a decent way.
+// The rendering works entirely through CSS.
+
 const sceneEl = document.querySelector(".scene");
 
 const {
@@ -21,7 +23,7 @@ const CONFIG = {
 	wireframe: false,
 	rotate: true,
 };
-const UPDATE = () => {
+const updateGUI = () => {
 	Object.entries(CONFIG).forEach(([key, value]) => {
 		document.documentElement.style.setProperty(`--${key}`, typeof value === "boolean" ? Number(value) : value);
 	});
@@ -29,48 +31,57 @@ const UPDATE = () => {
 };
 CONTROLLER.add(CONFIG, "wireframe")
 	.name("Show wireframe")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 const PLANE_FOLDER = CONTROLLER.addFolder("Camera");
 PLANE_FOLDER.add(CONFIG, "rotate-x", -360, 360, 1)
 	.name("Rotate X (deg)")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 PLANE_FOLDER.add(CONFIG, "rotate-y", -360, 360, 1)
 	.name("Rotate Y (deg)")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 const LEGS_FOLDER = CONTROLLER.addFolder("Legs");
 LEGS_FOLDER.add(CONFIG, "leg-width", 0, 15, 0.01)
 	.name("Leg Width")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 LEGS_FOLDER.add(CONFIG, "leg-skew", 0, 15, 0.01)
 	.name("Power Stance")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 // LEGS_FOLDER.add(CONFIG, 'left-leg-rotate-x', -90, 90, 1)
 //	.name('Left Leg Rotate X (deg)')
-//	.onChange(UPDATE);
+//	.onChange(updateGUI);
 LEGS_FOLDER.add(CONFIG, "left-leg-rotate-y", -90, 68, 1)
 	.name("Left Leg Rotate (deg)")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 // LEGS_FOLDER.add(CONFIG, 'right-leg-rotate-x', -90, 90, 1)
 //	.name('Left Leg Rotate X (deg)')
-//	.onChange(UPDATE);
+//	.onChange(updateGUI);
 LEGS_FOLDER.add(CONFIG, "right-leg-rotate-y", -90, 68, 1)
 	.name("Left Leg Rotate (deg)")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 // CONTROLLER.add(CONFIG, 'rotate')
 //	.name('Rotate face')
-//	.onChange(UPDATE);
+//	.onChange(updateGUI);
 CONTROLLER.add(CONFIG, "smile", 0, 1, 0.01)
 	.name("Smile")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 CONTROLLER.add(CONFIG, "eye-x", 0, 1, 0.01)
 	.name("Shifty Eyes")
-	.onChange(UPDATE);
+	.onChange(updateGUI);
 CONTROLLER.add(CONFIG, "lid-open")
 	.name("Lid Open")
-	.onChange(UPDATE);
-UPDATE();
+	.onChange(updateGUI);
+updateGUI();
 
 let mouseLastX, mouseLastY;
+const onMouseMove = (e) => {
+	const deltaX = e.clientX - mouseLastX;
+	const deltaY = e.clientY - mouseLastY;
+	CONFIG["rotate-x"] -= deltaY / 3;
+	CONFIG["rotate-y"] += deltaX / 3;
+	mouseLastX = e.clientX;
+	mouseLastY = e.clientY;
+	updateGUI();
+};
 sceneEl.addEventListener("mousedown", (event) => {
 	addEventListener("mousemove", onMouseMove, false);
 	mouseLastX = event.clientX;
@@ -80,15 +91,6 @@ sceneEl.addEventListener("mousedown", (event) => {
 addEventListener("mouseup", () => {
 	removeEventListener("mousemove", onMouseMove, false);
 });
-function onMouseMove(e) {
-	const deltaX = e.clientX - mouseLastX;
-	const deltaY = e.clientY - mouseLastY;
-	CONFIG["rotate-x"] -= deltaY / 3;
-	CONFIG["rotate-y"] += deltaX / 3;
-	mouseLastX = e.clientX;
-	mouseLastY = e.clientY;
-	UPDATE();
-}
 sceneEl.addEventListener("contextmenu", (event) => {
 	event.preventDefault();
 });
@@ -98,12 +100,14 @@ sceneEl.addEventListener("wheel", (event) => {
 	zoom += (event.deltaY < 0) ? 0.1 : -0.1;
 	document.documentElement.style.setProperty(`--zoom`, zoom);
 });
-function animate() {
+
+/*
+const animate = () => {
 	requestAnimationFrame(animate);
 	const t = performance.now();
 	CONFIG["left-leg-rotate.y"] = Math.sin(t / 500);
 	CONFIG["right-leg-rotate.y"] = -Math.sin(t / 500);
-	UPDATE();
-}
-// animate()
-
+	updateGUI();
+};
+animate();
+*/
