109966ff8c6a85c6e451a9c98a62b1d2999d34d4
Isaiah Odhner <isaiahodhner@gmail.com>
2022-04-05 17:52:17 -0400

    Move 3D renderer JS to external file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@
 module.exports = {
 	"env": {
 		"browser": true,
-		"es2021": true
+		"es2021": true,
 	},
 	"extends": "eslint:recommended",
 	"parserOptions": {
-		"ecmaVersion": 12
+		"ecmaVersion": 12,
+		"sourceType": "module",
 	},
 	"rules": {
 		"accessor-pairs": "error",
